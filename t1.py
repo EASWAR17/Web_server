efe288b1a0d06bfb53ebdd638d4ec71e48975a08
EASWAR17 <easwar.janakiraman@gmail.com>
2021-11-19 16:23:16 +0530

    python_web_design

---
+++ t1.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<h1>EASWAR</h1>
+<h2>21002130</h2>
+<h3>easwar.janakiraman@gmail.com</h3>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8080)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
